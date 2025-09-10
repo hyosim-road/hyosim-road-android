0e15a360d7738e06b912becdb15c1d21896d2f5f
zwongraphic <bababaenjooo@gmail.com>
2025-09-11 08:00:34 +0900

    refactor: setting

--- app/src/main/java/com/hyosim/hamkkae/presentation/main/setting/SettingFragment.kt
+++ app/src/main/java/com/hyosim/hamkkae/presentation/main/setting/SettingFragment.kt
@@ -1,10 +1,12 @@
 package com.hyosim.hamkkae.presentation.main.setting
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.hyosim.hamkkae.R
@@ -41,6 +43,14 @@ class SettingFragment: Fragment() {
             navigate(R.id.action_settingFragment_to_resignationFragment)
         }
 
+        binding.tvService.setOnClickListener {
+            navigate(R.id.action_settingFragment_to_signup_service)
+        }
+
+        binding.tvPersonal.setOnClickListener {
+            navigate(R.id.action_settingFragment_to_signup_personal)
+        }
+
         binding.cvLogout.setOnClickListener {
             val sessionManager = SessionManager
             sessionManager.clearToken()
@@ -53,10 +63,26 @@ class SettingFragment: Fragment() {
             requireActivity().finishAffinity()
         }
 
+        setSwitchState(binding.switchQuestion.isChecked)
+        binding.switchQuestion.setOnCheckedChangeListener { _, isChecked ->
+            setSwitchState(isChecked)
+        }
     }
 
     private fun navigate(resId:Int){
         findNavController().navigate(resId)
+    }
+
+    private fun setSwitchState(isChecked: Boolean) {
+        if (isChecked) {
+            binding.switchQuestion.trackTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(requireContext(), R.color.hover_orange)
+            )
+        } else {
+            binding.switchQuestion.trackTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(requireContext(), R.color.auth_gray)
+            )
+        }
     }
 
     override fun onDestroyView() {
